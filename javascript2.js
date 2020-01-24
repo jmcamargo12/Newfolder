14f760c239f3a29348a7dda4ebb08903002080fd
jonathan manuel camargo labeau <jmcamargo12@gmail.com>
2020-01-24 02:16:53 -0500

    seventh

--- javascript2.js
+++ javascript2.js
@@ -1,7 +1,9 @@
-var welcome = "Hi my name is Jonathan, but people around here call me Mc.Fly.";
+var wel = "Hi and Welcome!";
+alert(wel);
+var welcome = "My name is Jonathan, but people around here call me Mc.Fly!";
 alert(welcome);
 
-var declaration = "I developed this quiz you are about to take.";
+var declaration = "Im the developer of this quiz you are about to take.";
 alert(declaration);
 
 var userName = prompt("What's your name?");
@@ -10,11 +12,11 @@ alert(
     userName +
     " I know someone called " +
     userName +
-    ", always in a good mood and super smart that " +
+    ". Real smart, loves pizza and always has smelly feet, our close friend call that " +
     userName +
-    " I knew"
+    "boberts the pippen Dixon.."
 );
 
-var ready = "ready for the quiz?";
+var ready = confirm("Anyways...are you ready for the quiz?");
 alert("May the force be with you " + userName);
-alert("always!");
+alert("Always!");
