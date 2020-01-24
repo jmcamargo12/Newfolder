02291dc3d675fa242c26db335690cdf0ea4dd356
jonathan manuel camargo labeau <jmcamargo12@gmail.com>
2020-01-24 02:04:11 -0500

    sixth

---
+++ javascript2.js
@@ -0,0 +1,20 @@
+var welcome = "Hi my name is Jonathan, but people around here call me Mc.Fly.";
+alert(welcome);
+
+var declaration = "I developed this quiz you are about to take.";
+alert(declaration);
+
+var userName = prompt("What's your name?");
+alert(
+  "cool name " +
+    userName +
+    " I know someone called " +
+    userName +
+    ", always in a good mood and super smart that " +
+    userName +
+    " I knew"
+);
+
+var ready = "ready for the quiz?";
+alert("May the force be with you " + userName);
+alert("always!");
